eee6d740615babe4af9063da24d10b8de5e00bc1
Loriano Storchi <loriano@storchi.org>
2018-03-28 14:20:42 +0200

    backup

--- analyse.py
+++ analyse.py
@@ -1,3 +1,4 @@
+import openbabel
 import argparse
 import pybel
 import math
@@ -7,12 +8,22 @@ import os
 
 from operator import itemgetter
 
+###############################################################################
+
+def if_exist_rm (fname):
+    if os.path.exists(fname):
+        os.remove(fname)
+
+###############################################################################
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-z","--minz", help="minimum difference along z", type=float,\
-                default=10.0)
+                default=5.0)
 parser.add_argument("-x","--minx", help="minimum difference along x", type=float,\
                 default=5.0)
 parser.add_argument("-y","--miny", help="minimum difference along y", type=float,\
+                default=5.0)
+parser.add_argument("-d","--mind", help="minimum distance", type=float,\
                 default=5.0)
 
 
@@ -21,12 +32,17 @@ args = parser.parse_args()
 minx = args.minx
 miny = args.miny
 minz = args.minz
+mind = args.mind
 
 os.chdir("./")
+
+if_exist_rm ("final.txt")
+fp = open("final.txt", "w")
 
 for file in glob.glob("*.pdb"):
     print(file)
     mol = list(pybel.readfile("pdb", file))
+    basename = file[:-4]
 
     if len(mol) != 1:
         print "Each file should contain a single molecule set"
@@ -47,22 +63,27 @@ for file in glob.glob("*.pdb"):
         wire = []
         wire.append(lastidx)
         for i in range(1,len(sort_acoordslist)):
-            diffz = sort_acoordslist[i][2] - \
-                    lastatom[2]
+            diffx = math.fabs(sort_acoordslist[i][0] - \
+                    lastatom[0])
+            diffy = math.fabs(sort_acoordslist[i][1] - \
+                    lastatom[1])
+            diffz = math.fabs(sort_acoordslist[i][2] - \
+                    lastatom[2])
+ 
+            dist = math.sqrt(diffx**2 + diffy**2 + diffz**2)
             wirecompleted = True
-            if (diffz <= minz):
-                diffx = math.fabs(sort_acoordslist[i][0] - \
-                        lastatom[0])
-                diffy = math.fabs(sort_acoordslist[i][1] - \
-                        lastatom[1])
-
-                if (diffx <= minx) and (diffy <= miny):
+            if (diffz <= minz) and (diffx <= minx) and (diffy <= miny) \
+                    and (dist <= mind):
                     wire.append(i)
                     wirecompleted = False
 
             if wirecompleted:
                 wires.append(wire)
                 wire = []
+                lastidx = i
+                lastatom = sort_acoordslist[lastidx]
+                wire.append(lastidx)
+            else:
                 lastidx = i
                 lastatom = sort_acoordslist[lastidx]
 
@@ -80,5 +101,33 @@ for file in glob.glob("*.pdb"):
                 wires.append(wire)
 
         print "Num. of wires: ", len(wires)
+        tot = 0
+        molidx = 1
         for wire in wires:
             print "   ", len(wire)
+            fp.write(str(len(wire)) + "\n")
+            tot += len(wire)
+
+            obmol = openbabel.OBMol()
+            lidx = 0
+            for idx in wire:
+                newobatom = openbabel.OBAtom()
+                newobatom.SetAtomicNum(8)
+                newobatom.SetId(lidx)
+                newobatom.SetIdx(lidx)
+                newobatom.SetVector(sort_acoordslist[idx][0], \
+                        sort_acoordslist[idx][1], sort_acoordslist[idx][2]) 
+                obmol.AddAtom(newobatom)
+                lidx = lidx + 1
+
+            for idx in range(1,len(wire)):
+                obmol.AddBond(idx-1, idx, 1)
+
+            pybelmol = pybel.Molecule(obmol)
+            pybelmol.write("mol2", basename + "_" + str(molidx)+".mol2", True)
+            molidx = molidx + 1
+
+        if tot != len(sort_acoordslist):
+            print "error in total number"
+
+fp.close()
