a1b08ef154a6a42a2c0cd6902938f5b2b829a352
Loriano Storchi <loriano@storchi.org>
2018-03-28 12:15:46 +0200

    added basi code to compute wire sets

--- analyse.py
+++ analyse.py
@@ -1,5 +1,6 @@
 import argparse
 import pybel
+import math
 import glob 
 import sys
 import os
@@ -7,10 +8,19 @@ import os
 from operator import itemgetter
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-x","--axis", help="axis to look for", type=str,\
-                default="z")
+parser.add_argument("-z","--minz", help="minimum difference along z", type=float,\
+                default=10.0)
+parser.add_argument("-x","--minx", help="minimum difference along x", type=float,\
+                default=5.0)
+parser.add_argument("-y","--miny", help="minimum difference along y", type=float,\
+                default=5.0)
+
 
 args = parser.parse_args()
+
+minx = args.minx
+miny = args.miny
+minz = args.minz
 
 os.chdir("./")
 
@@ -28,5 +38,47 @@ for file in glob.glob("*.pdb"):
             acoordslist.append(a.coords)
 
     sort_acoordslist = sorted(acoordslist, key=itemgetter(2))
-    for a in sort_acoordslist:
-        print a
+
+    if (len(sort_acoordslist) > 1):
+        lastidx = 0
+        lastatom = sort_acoordslist[lastidx]
+
+        wires = []
+        wire = []
+        wire.append(lastidx)
+        for i in range(1,len(sort_acoordslist)):
+            diffz = sort_acoordslist[i][2] - \
+                    lastatom[2]
+            wirecompleted = True
+            if (diffz <= minz):
+                diffx = math.fabs(sort_acoordslist[i][0] - \
+                        lastatom[0])
+                diffy = math.fabs(sort_acoordslist[i][1] - \
+                        lastatom[1])
+
+                if (diffx <= minx) and (diffy <= miny):
+                    wire.append(i)
+                    wirecompleted = False
+
+            if wirecompleted:
+                wires.append(wire)
+                wire = []
+                lastidx = i
+                lastatom = sort_acoordslist[lastidx]
+
+        if len(wires) == 0:
+            wires.append(wire)
+        else:
+            lastwire = wires[-1]
+            areequal = False
+            if len(wire) == len(lastwire):
+                valmatch = [i for i, j in zip(wire, lastwire) if i == j]
+                if valmatch == len(wire):
+                    areequal = True
+
+            if not areequal:
+                wires.append(wire)
+
+        print "Num. of wires: ", len(wires)
+        for wire in wires:
+            print "   ", len(wire)
